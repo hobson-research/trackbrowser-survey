a68a08c4ac0d5af7aaa3a00f38f4958373998261
Ye Joo Park <subwaymatch@gmail.com>
2017-09-20 07:27:32 -0700

    Fix pylint warning messages

--- main.py
+++ main.py
@@ -1,61 +1,64 @@
 from pymongo import MongoClient
+from flask import Flask
+
+app = Flask(__name__)
 
 list_users = ['Bubble', 'Dargude2', 'kakakyle0214 ', 'Katy', 'Lewyreus', 'Liangwenqian93 ', 'Locust', 'Naruto', 'Pommel9', 'rookie', 'ssshah10', 'tanvirotkar', 'ventra', 'zlow4', 'zsosa']
 
 # MongoDB connection
-client = MongoClient('localhost', 27017)
-db = client.trackbrowser
-
-# MongoDB aggregation query pipeline to randomly sample a navigation
-pipeline = [
-	{
-		"$match": {
-			"userName": list_users[0],
-			"type": "navigation"
-		}
-	},
-	{
-		"$sample": { 
-			"size": 1
-		}
-	}
-]
+DB_CLIENT = MongoClient('localhost', 27017)
+DB = DB_CLIENT.trackbrowser
 
 
 def main():
+	# MongoDB aggregation query pipeline to randomly sample a navigation
+	pipeline = [
+		{
+			"$match": {
+				"userName": list_users[0],
+				"type": "navigation"
+			}
+		},
+		{
+			"$sample": { 
+				"size": 1
+			}
+		}
+	]
+
 	# Randomly select a navigation entry
 	# MongoDB's aggregate query is used
-	for nav in db.browsing_data.aggregate(pipeline):
-		randomNavigation = nav	
+	for nav in DB.browsing_data.aggregate(pipeline):
+		random_navigation = nav	
 	
 	
 	# Based on the selected navigation, find the next navigation entry based on time
-	nextNavigation = db.browsing_data.find_one({
-		"userName": randomNavigation.get("userName"), 
+	next_navigation = DB.browsing_data.find_one({
+		"userName": random_navigation.get("userName"), 
 		"type": "navigation", 
-		"tabViewId": randomNavigation.get("tabViewId"), 
+		"tabViewId": random_navigation.get("tabViewId"), 
 		"timestamp": {
-			"$gt": randomNavigation.get("timestamp")
+			"$gt": random_navigation.get("timestamp")
 		}
 	})
 
 	# Find screenshot entries for the selected navigation event
-	screenshots = db.browsing_data.find({
-		"userName": randomNavigation.get("userName"), 
+	screenshots = DB.browsing_data.find({
+		"userName": random_navigation.get("userName"), 
 		"type": "screenshot", 
-		"tabViewId": randomNavigation.get("tabViewId"), 
+		"tabViewId": random_navigation.get("tabViewId"), 
 		"timestamp": {
-			"$gt": randomNavigation.get("timestamp"), 
-			"$lt": nextNavigation.get("timestamp")
+			"$gt": random_navigation.get("timestamp"), 
+			"$lt": next_navigation.get("timestamp")
 		}
 	})
 
 	# Query screenshot
-	print(randomNavigation)
-	print(nextNavigation)
+	print(random_navigation)
+	print(next_navigation)
 
 	# Screenshots
-	print("Screenshot(s)")
+	print(screenshots.count(), "screenshot(s)")
 	
 	for screenshot in screenshots: 
 		print(screenshot)
